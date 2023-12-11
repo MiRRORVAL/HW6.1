3a2c15406cd087330fb16f983c2aa46e14b217e1
Nur <mirrorvol@gmail.com>
2023-12-11 19:01:30 +0400

    Initial Commit

---
+++ HW6.1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW6.1
+//
+//  Created by Nur on 11.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
